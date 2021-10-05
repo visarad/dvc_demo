d395209d2d0f0939ef273e87b2e916e9a35be5dc
visarad <visarad@gmail.com>
2021-10-05 08:52:01 +0530

    committing all files

---
+++ stage_01.py
@@ -0,0 +1,12 @@
+text = 'this is the only outcome of this file '
+print('===================================')
+print('=========START OF STAGE 01=========')
+print('===================================')
+
+with open('artifacts_01.txt','w') as f:
+    f.write(text)
+
+print('===================================')
+print('==========END OF STAGE 01==========')
+print('===================================')
+
